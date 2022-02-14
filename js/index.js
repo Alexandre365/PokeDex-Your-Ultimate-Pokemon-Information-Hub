87fe1946ba32ccbd4b24a35df24fe2d5a52be340
Alexandre <tube998@gmail.com>
2022-02-14 01:09:19 -0300

    V1.1
    Input

--- js/index.js
+++ js/index.js
@@ -36,10 +36,35 @@ const FetchPokemon = () =>{
                 </button>`
                 return accumulator
             },'')
-        document.getElementById('List-Card').innerHTML = lisPokemons
-        })   
+            const input = document.getElementById('search');
+            input.addEventListener('input', updateValue);
 
-    
+            if(input.value == ''){
+                document.getElementById('List-Card').innerHTML = lisPokemons
+            }
+            function updateValue(e) {
+                let list = pokemons.filter(function(id){
+                    return id.name.indexOf(`${e.target.value}`.toLowerCase()) > -1
+                })
+                document.getElementById('List-Card').innerHTML = list.reduce((accumulator, pokemons)=>{
+                    accumulator += `
+                    <button class="Card" onclick="clr(${pokemons.id})">
+                        <div class='Card-poke'>
+                            <div class="card-poke-img">
+                                <img src="${pokemons.sprites.front_default}" >
+                            </div>
+                            <div class="card-txt">
+                                <h2>${Name(pokemons.name)}</h2>
+                                <p>#${IdNumero(pokemons.id)}</p>
+                            </div>
+                        </div>
+                    </button>`
+                    return accumulator 
+                   },'') 
+            }
+            
+        })   
+       
 }
 
 FetchPokemon()
